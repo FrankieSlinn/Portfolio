3bc11fb1eedd35d12d2aa30fbf5f4c79b7e36a23
Franziska Kissling <frankissling@yahoo.co.uk>
2023-03-26 22:18:27 +0100

    Text and timing changes

--- control.js
+++ control.js
@@ -2,14 +2,14 @@ var i = 0;
 var j = 0;
 var firstDone = "no"
 
-let txt1 = "Enthusiastic Junior Programmer. Attended the General Assembley Software Engineering Immersive Course and has industry coding experience. Programming Skills: Javascript, APIs, Python, CSS, HTML, GIT. Creator of multiple applications."
-let txt2 = "Masters Graduate in Financial Risk and Investment Anaysis at the University of Sussex. Obtained a Distinction. Experienced in Business Systems Analysis, Project Management, Functional and Accessibility Testing via employers LTG and American Express. Familiar with WCAG Standards."
+let txt1 = "Enthusiastic Junior Programmer**Attended the General Assembley Software Engineering Immersive Course Q2 2023**Coding experience on the job at GP Strategies(E-Learning) in HTML, CSS, JS**Programming Skills: Javascript, APIs, Python, CSS, HTML, GIT** Creator of multiple applications."
+let txt2 = "Masters Graduate in Financial Risk and Investment Anaysis at the University of Sussex - Received Distinction**Experienced in Business Systems Analysis**Lead Project Manager in American Express**Functional and Accessibility Testing at GP Strategies**Familiar with WCAG Standards**Performed in over 350 standup comedy shows."
 
 var speed =60; /* The speed/duration of the effect in milliseconds */
 
 typeWriter(txt1, ".first", i);
 //Ensures the 2nd paragraph is not populated at the same time as the first paragraph
-setTimeout(() => typeWriter(txt2, ".second", j), 18000)
+setTimeout(() => typeWriter(txt2, ".second", j), 20000)
 function typeWriter(txt, location, param, func) {
     
     if(param < txt.length) {
